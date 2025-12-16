891b3e95e4fcb6ba545ada0314c61e484c1e0d90
Farya Hussain <72506658+faryaHussain@users.noreply.github.com>
2025-12-16 22:51:43 +0400

    Add snippet from Dev Snippets

---
+++ snippets/onesnip.dart
@@ -0,0 +1,9 @@
+bool get isHovered => contains(WidgetState.hovered);
+        
+        
+          
+            bool get isFocused => contains(WidgetState.focused);
+        
+        
+          
+            bool get isPressed => contains(WidgetState.pressed);
